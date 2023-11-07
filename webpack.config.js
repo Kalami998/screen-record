587bd5a5d6a99949d91434d9c58974d6aa0d7506
黄泽民 <467251648@qq.com>
2023-11-07 15:23:12 +0800

    更新icon

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,10 @@ module.exports = {
 	entry: "./index.js", // 入口文件
 	output: {
 		path: path.resolve(__dirname, "./dist"), // 输出文件夹
-		filename: "screen-webrtc.min.js", // 文件名称
+		filename: "screen-record.min.js", // 文件名称
 		libraryTarget: "umd", // 打包方式
 		globalObject: "this", // 全局对象
-		library: "screen-webrtc", // 类库名称
+		library: "screen-record", // 类库名称
 	},
 	plugins: [
 		new CleanWebpackPlugin(), // 清除上一次打包内容
